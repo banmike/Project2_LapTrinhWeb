46bddb3b3afe0a439260e65150c5016e34d217b9
Nguyen Khanh <131245621+banmike@users.noreply.github.com>
2024-03-03 09:34:19 +0700

    Add files via upload

---
+++ make-multi-filter.js
@@ -0,0 +1,19 @@
+'use strict';
+function MakeMultiFilter(originalArray)
+{
+    var currentArray = originalArray;
+    function arrayFilterer(filterCriteria, callback){
+        if( typeof filterCriteria !== "function")
+        {
+            return currentArray;
+        }
+        currentArray=currentArray.filter(filterCriteria);
+        if( typeof callback === "function")
+        {
+            callback.call(originalArray, currentArray);
+        
+        }
+            return arrayFilterer;
+    }
+    return arrayFilterer;
+}
